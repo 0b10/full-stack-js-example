eaf15d4e39f505a1673c2497290b17646dc7de78
0b10 <0b10@pm.me>
2019-04-18 17:30:53 +0100

    Add eslint and run lint

--- server/db/knex/connection.js
+++ server/db/knex/connection.js
@@ -13,10 +13,12 @@ const envConfig = config[environment];
 // Make the actual DB connection. This runs only once due to Node's module caching.
 const connection = knex(envConfig); // The connection is pooled by default.
 
-if (connection) console.log(
-  `* Database connection made with NODE_ENV: ${environment} \
-  to database ${envConfig.connection.database}.`
-); // Practice project, just for debugging.
+if (connection) {
+  console.log(
+    `* Database connection made with NODE_ENV: '${environment}', \
+    to database '${envConfig.connection.database}'.`,
+  ); // Practice project, just for debugging.
+}
 
 // Export the connection.
 // Node's module caching means that it is the same instance that is exported when it's used in
